8e6ac9c5cc4930cd29d64d676f27719d8d1732f4
adkow <a.kowalewski.student@gmail.com>
2021-12-12 20:22:09 +0100

    Add to favorites/ remove from favorites functionality

--- lib/book_details.dart
+++ lib/book_details.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:provider/provider.dart';
 
 import './comments.dart';
 import './book.dart';
+import './user_state.dart';
 
 class BookDetails extends StatefulWidget {
   final int id;
@@ -37,6 +39,15 @@ class BookDetailsState extends State<BookDetails> {
   bool _loadingComments = false;
   String _commentsError = "";
   bool _comms = false;
+  bool? _isFavorite;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) {
+      isFavorite();
+    });
+  }
 
   Widget BookInfo() {
     return Column(children: [
@@ -79,11 +90,70 @@ class BookDetailsState extends State<BookDetails> {
     ]));
   }
 
+  void toggleFavorite() async {
+    var response;
+    try {
+      final token = Provider.of<UserState>(context, listen: false).token;
+      response = await http.put(
+          Uri.parse('http://10.0.2.2:8000/api/users/favorite/${widget.id}'),
+          headers: <String, String>{
+            'Authorization': 'Bearer $token',
+          });
+    } catch (e) {}
+
+    if (response.statusCode >= 200 && response.statusCode < 300) {
+      setState(() {
+        _isFavorite = !_isFavorite!;
+      });
+    } else {
+      debugPrint('NIE POSZŁO');
+    }
+  }
+
+  void isFavorite() async {
+    var response;
+    final token = Provider.of<UserState>(context, listen: false).token;
+    response = await http.get(
+        Uri.parse('http://10.0.2.2:8000/api/users/favorite/${widget.id}'),
+        headers: <String, String>{
+          'Authorization': 'Bearer $token',
+        });
+    Map<String, dynamic> jsonData = await jsonDecode(response.body);
+    if (jsonData['fav'] == true) {
+      setState(() {
+        _isFavorite = true;
+      });
+    } else {
+      setState(() {
+        _isFavorite = false;
+      });
+    }
+  }
+
+  Widget Favorites_star() {
+    return IconButton(
+      icon: _isFavorite!
+          ? Icon(
+              Icons.star,
+              color: Colors.yellow,
+            )
+          : Icon(
+              Icons.star_border,
+              color: Colors.yellow,
+            ),
+      tooltip: 'Add to favorites',
+      onPressed: () {
+        toggleFavorite();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text(widget.title),
+          actions: <Widget>[Favorites_star()],
         ),
         body: SingleChildScrollView(
             child: Column(
